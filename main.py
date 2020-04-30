6525dea11286f61563b7340f0651dd9c930732c5
Pashs-ba <pashs.ba@gmail.com>
2020-04-30 14:08:53 +0300

    finish program

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import psutil
 import serial
+import datetime
 import time
 
 
-class PCMonitor:
-    def __init__(self):
-        self.ser = serial.Serial('/dev/ttyUSB' + self.find_open_port(), 9600)  # find open serial port
+def find_open_port():
+    """Find open serial port"""
+    ports = ['0', '1', '2', '3']
+    while True:  # need to run program in tree while all port close
+        for port in ports:
+            try:
+                serial.Serial('/dev/ttyUSB' + port, 9600)
+                return port
+            except:
+                pass
 
-    @staticmethod
-    def find_open_port():
-        ports = ['0', '1', '2', '3']
-        while True:  # need to run program in tree while devise off
-            for port in ports:
-                try:
-                    serial.Serial('/dev/ttyUSB' + port, 9600)
-                    return port
-                except:
-                    pass
+
+def main():
+    while True:
+        ser = serial.Serial('/dev/ttyUSB' + find_open_port(), 9600)
+        try:  # if devise is off
+            while True:
+                proc = max(psutil.cpu_percent(interval=1, percpu=True))  # get most loaded core
+                ram = psutil.virtual_memory().used/psutil.virtual_memory().total*100  # percents of ram using
+                now = datetime.datetime.now()  # now time
+                ser.write(str(str(int(proc)) + ' ').encode())
+                ser.write(str(str(int(ram)) + ' ').encode())
+                ser.write(str(str(int(now.hour)) + ' ').encode())
+                ser.write(str(int(now.minute)).encode())
+                time.sleep(1)
+        except:
+            pass
 
 
 if __name__ == '__main__':
-    pass
+    main()
